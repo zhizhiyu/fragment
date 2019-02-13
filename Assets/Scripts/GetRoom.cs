2c52e575ac72514ea9bc4d9f873e92eb204daf8b
Mario8664 <pooh3000@126.com>
2019-02-14 02:04:33 +0800

    拼图部分

---
+++ Assets/Scripts/GetRoom.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+/*这段脚本用来点击获取拼图房间*/
+[RequireComponent(typeof(Camera))]
+public class GetRoom : MonoBehaviour
+{
+    public float unitLength; //网格的单位长度
+    Camera myCamera; //正在使用的摄像机
+    RaycastHit2D room; //拖动的房间
+    Ray ray; //鼠标发出的用以获取房间的射线
+
+    void Start()
+    {
+        myCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        GetTheRoom();
+    }
+
+    void GetTheRoom()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            try
+            {
+                ray = myCamera.ScreenPointToRay(Input.mousePosition);
+                room = Physics2D.Raycast(ray.origin, ray.direction);
+                var moveRoom = room.collider.gameObject.GetComponent<MoveRoom>();
+                if (moveRoom != null)
+                {
+                    moveRoom.unreal.SetActive(true);
+                    moveRoom.unit = unitLength;
+                    moveRoom.mousePosition = ray.origin - room.transform.position;
+                    moveRoom.myCamera = myCamera;
+                    moveRoom.enabled = true;
+                }
+            }
+            catch (System.NullReferenceException e)
+            {
+                #if UNITY_EDITOR
+                Debug.Log("No Room");
+                #endif
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MoveRoom.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+/*这段脚本用来移动房间*/
+[DisallowMultipleComponent]
+public class MoveRoom : MonoBehaviour
+{
+    public GameObject real;//真实的房间
+    public GameObject unreal;//用以提示房间将要落下的位置的房间
+    [HideInInspector] public float unit;//网格单位长度
+    public Vector2 mousePosition;//鼠标点击的房间位置偏差
+    [HideInInspector] public Camera myCamera;//正在使用的摄像机
+    private MoveRoom self;//组件自身
+    void Start()
+    {
+        self = GetComponent<MoveRoom>();
+    }
+
+    void Update()
+    {
+        MoveTheRoom();
+    }
+
+    void MoveTheRoom()
+    {
+        Vector2 targetPosition = (Vector2)myCamera.ScreenPointToRay(Input.mousePosition).origin - mousePosition;
+        var realTransform = real.GetComponent<Transform>();
+        realTransform.position = targetPosition;
+        var unrealTransform = unreal.GetComponent<Transform>();
+        //计算网格对齐坐标
+        float x = Mathf.Floor(targetPosition.x / unit + 0.5f) * unit;
+        float y = Mathf.Floor(targetPosition.y / unit + 0.5f) * unit;
+        //计算完毕
+        unrealTransform.position = new Vector2(x, y);
+        if (Input.GetButtonUp("Fire1"))
+        {
+            realTransform.position = unrealTransform.position;
+            unreal.SetActive(false);
+            self.enabled = false;
+        }
+    }
+}
